2c207c385fb1dfdd3fd2e1d8a262b98725b77446
Kristjan Luik <kristjanluik1@gmail.com>
2016-10-10 22:50:18 +0300

    Form validation.

--- csv_app/routes/upload.js
+++ csv_app/routes/upload.js
@@ -2,13 +2,34 @@ var express = require('express');
 var router = express.Router();
 var ejs = require('ejs');
 var formidable = require('formidable');
-
+var validator = require('validator');
+var fs = require('fs');
 
 router.post('/', function(req, res, next) {
     var form = new formidable.IncomingForm();
     form.uploadDir = "public/uploads";
+
     form.parse(req, function(err, fields, files) {
-        res.redirect('/processcsv' + "?filepath=" + files.upload.path +"&&email="+fields.email +"&&date="+fields.date);
+        var error = {email:false, date: false, file: false};
+        if (validator.isEmpty(fields.email) || !validator.isEmail(fields.email)) {
+            //Validate email
+            error.email = true;
+        }else if (validator.isEmpty(fields.date) || !validator.isDate(fields.date)) {
+            error.date = true;
+        //}else if (!validator.isEmpty(files.upload.File.name) || files.upload.File.name.match(/\.(csv)$/i)) {
+        }else if (files.upload.name == '' /*|| !files.upload.File.name.match(/\.(csv)$/i)*/) {
+            error.file = true;
+        }
+
+        if(error.email || error.date || error.file){
+            //There was an error
+            res.redirect('/');
+            fs.unlink(files.upload.path, function (err, res) {
+            });
+        }else {
+            //There was no error
+            res.redirect('/processcsv' + "?filepath=" + files.upload.path +"&&email="+fields.email +"&&date="+fields.date);
+        }
     });
 });
 
